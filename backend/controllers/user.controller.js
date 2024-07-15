c817a52d0de02b4a0b1dd10c9a02dbfb0dd1f814
mayurhapani <hapanimayur@gmail.com>
2024-07-15 15:14:07 +0530

    added json msg in response

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,11 +4,16 @@ const bcrypt = require("bcrypt");
 const signup = async (req, res) => {
   try {
     const { name, username, email, password } = req.body;
+    if (!name || !username || !email || !password) {
+      return res.status(422).json({ message: "Invalid input" });
+    }
+
     const existingUser = await userModel.findOne({ $or: [{ email }, { username }] });
 
     if (existingUser) {
-      return res.status(422).send();
+      return res.status(422).json({ message: "User already exists" });
     }
+    console.log(name, username, email, password);
 
     bcrypt.hash(password, 10, async (err, hash) => {
       await userModel.create({
