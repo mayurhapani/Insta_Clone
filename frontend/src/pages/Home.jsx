70e125a1b7eafa32d0d0b69d60d28239cce92557
mayurhapani <hapanimayur@gmail.com>
2024-07-16 16:11:40 +0530

    profile page done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,51 +1,53 @@
-import img1 from "../assets/images/demo user.png";
+import img1 from "../assets/images/demo_user.png";
 
 export default function Home() {
   return (
-    <div className="pt-32 flex flex-col items-center">
-      <div className=" w-1/2 rounded-sm">
-        <div className="card border border-[rgb(173, 173, 173)] rounded-sm">
-          {/* card header */}
-          <div className="flex justify-start items-center p-2">
-            <img className="w-[40px] rounded-full me-5" src={img1} alt="" />
-            <span className="font-semibold">user name</span>
-          </div>
-
-          {/* card post image */}
-          <div className="">
-            <img className="w-full h-max" src={img1} alt="" />
-          </div>
-
-          {/* card content */}
-          <div className="py-2 px-1 flex justify-between items-center border border-[rgb(173, 173, 173)]">
-            <div className="">
-              <span className="material-symbols-outlined me-2 text-2xl">favorite</span>
-              <span className="material-symbols-outlined me-2 text-2xl">maps_ugc</span>
-              <span className="material-symbols-outlined text-2xl">send</span>
+    <div className="container mx-auto ">
+      <div className="pt-32 flex flex-col items-center">
+        <div className=" w-1/2 rounded-sm">
+          <div className="card border border-[rgb(173, 173, 173)] rounded-sm">
+            {/* card header */}
+            <div className="flex justify-start items-center p-2">
+              <img className="w-[40px] rounded-full me-5" src={img1} alt="" />
+              <span className="font-semibold">user name</span>
             </div>
-            <span className="material-symbols-outlined text-2xl">bookmark</span>
-          </div>
 
-          {/* card footer */}
-          <div className="px-2">
-            <span className="pe-2">1</span>
-            <span>likes</span>
-          </div>
+            {/* card post image */}
+            <div className="">
+              <img className="w-full h-max" src={img1} alt="" />
+            </div>
 
-          {/* comments */}
-          <div className="p-2">
-            <p>This is Comments</p>
-          </div>
+            {/* card content */}
+            <div className="py-2 px-1 flex justify-between items-center border border-[rgb(173, 173, 173)]">
+              <div className="">
+                <span className="material-symbols-outlined me-2 text-2xl">favorite</span>
+                <span className="material-symbols-outlined me-2 text-2xl">maps_ugc</span>
+                <span className="material-symbols-outlined text-2xl">send</span>
+              </div>
+              <span className="material-symbols-outlined text-2xl">bookmark</span>
+            </div>
 
-          {/* add comments */}
-          <div className="flex items-center">
-            <span className="material-symbols-outlined">mood</span>
-            <input
-              className="outline-none border border-gray-200 p-1 text-sm rounded-lg w-full mx-2"
-              type="text"
-              placeholder="Add Comments..."
-            />
-            <button className="px-1 pb-2 text-lg text-blue-800 font-semibold">post</button>
+            {/* card footer */}
+            <div className="px-2">
+              <span className="pe-2">1</span>
+              <span>likes</span>
+            </div>
+
+            {/* comments */}
+            <div className="p-2">
+              <p>This is Comments</p>
+            </div>
+
+            {/* add comments */}
+            <div className="flex items-center">
+              <span className="material-symbols-outlined">mood</span>
+              <input
+                className="outline-none border border-gray-200 p-1 text-sm rounded-lg w-full mx-2"
+                type="text"
+                placeholder="Add Comments..."
+              />
+              <button className="px-1 pb-2 text-lg text-blue-800 font-semibold">post</button>
+            </div>
           </div>
         </div>
       </div>
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,24 +1,66 @@
-export default function Profile() {
+import img1 from "../assets/images/demo_user.png";
+ export default function Profile() {
   return (
-    <div className="pt-32 flex flex-col items-center">
-      <div className="flex items-center">
-        <img
-          className="rounded-full w-24 h-24"
-          src="https://via.placeholder.com/150"
-          alt="User Avatar"
-        />
-        <div className="ps-5">
-          <h1 className="text-2xl font-bold text-center mb-5 ">John Doe</h1>
+    <div className="container mx-auto ">
+      <div className="pt-32 flex flex-col items-center w-1/2 mx-auto">
+        {/* header */}
+        <div className="w-full">
           <div className="flex items-center">
-            <p className="text-sm text-gray-600 me-4">
-              <span>40</span> Posts
-            </p>
-            <p className="text-sm text-gray-600 me-4">
-              <span>40</span> Followers
-            </p>
-            <p className="text-sm text-gray-600">
-              <span>40</span> Following
-            </p>
+            <img
+              className="rounded-full w-32 h-32"
+              src="https://via.placeholder.com/150"
+              alt="User Avatar"
+            />
+            <div className="ps-5">
+              <h1 className="text-3xl font-bold text-center mb-5 ">John Doe</h1>
+              <div className="flex items-center">
+                <p className="text-sm text-gray-600 font-semibold me-4">
+                  <span>40</span> Posts
+                </p>
+                <p className="text-sm text-gray-600 font-semibold me-4">
+                  <span>40</span> Followers
+                </p>
+                <p className="text-sm text-gray-600 font-semibold">
+                  <span>40</span> Following
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <hr className="w-full mx-auto mt-2 mb-10" />
+
+        {/* posts */}
+        <div className=" flex flex-wrap">
+          <div className="w-1/3 p-1 ">
+            <div className="border border-gray-300">
+              <img className="w-full h-full" src={img1} alt="" />
+            </div>
+          </div>
+          <div className="w-1/3 p-1 ">
+            <div className="border border-gray-300">
+              <img className="w-full h-full" src={img1} alt="" />
+            </div>
+          </div>
+          <div className="w-1/3 p-1 ">
+            <div className="border border-gray-300">
+              <img className="w-full h-full" src={img1} alt="" />
+            </div>
+          </div>
+          <div className="w-1/3 p-1 ">
+            <div className="border border-gray-300">
+              <img className="w-full h-full" src={img1} alt="" />
+            </div>
+          </div>
+          <div className="w-1/3 p-1 ">
+            <div className="border border-gray-300">
+              <img className="w-full h-full" src={img1} alt="" />
+            </div>
+          </div>
+          <div className="w-1/3 p-1 ">
+            <div className="border border-gray-300">
+              <img className="w-full h-full" src={img1} alt="" />
+            </div>
           </div>
         </div>
       </div>
